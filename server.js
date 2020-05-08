8338cbefa5b0bf96c7ad71888f934adadc2282b0
Alexander Joharico <joharicoalexander@gmail.com>
2020-05-09 00:22:46 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app     = express();
+
+app.get('/', (req, res) => {
+   res.json('API is Works !');
+});
+
+
+
+
+const PORT  = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
