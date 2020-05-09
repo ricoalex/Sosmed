cf0862fd0fca1075e1825581772567ea0991f3dd
ricoalex <joharicoalexander@gmail.com>
2020-05-09 13:12:43 +0700

    Perubahan Server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on cusss port ${PORT}`);
 });
